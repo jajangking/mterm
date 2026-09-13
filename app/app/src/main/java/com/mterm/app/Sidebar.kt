f1523d6afabbd04d001602251368a934dce66c77
mterm dev <dev@mterm.local>
2026-09-13 15:35:51 +0700

    fix(ui): lineHeight pakai TextUnit (.sp)

--- app/app/src/main/java/com/mterm/app/Sidebar.kt
+++ app/app/src/main/java/com/mterm/app/Sidebar.kt
@@ -176,7 +176,7 @@ fun SidebarDrawer(
                         previewText!!,
                         fontFamily = FontFamily.Monospace,
                         fontSize = fontSp,
-                        lineHeight = fontSp * 1.25f,
+                        lineHeight = (fontSp * 1.25f).sp,
                         color = if (dark) Color(0xFFD6D6D6) else Color(0xFF222222),
                         modifier = Modifier.fillMaxSize(),
                     )
